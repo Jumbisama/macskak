4e70ec92d340ab946d83b7c5190ebe306e130077
Jumbi-sama <jambordavid2004@gmail.com>
2023-05-17 11:26:00 +0200

    10 teny

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function kiir() {
     fetch("https://catfact.ninja/fact")
     .then(adat => adat.json())
     .then(res =>{
-        document.getElementById(teny).innerHTML = res["fact"];
+        document.getElementById(teny).innerHTML += `<li>${item["fact"]} </li>`
     })
 }
